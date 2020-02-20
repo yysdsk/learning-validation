1688bafeb422c34776dc48fd126566f69c7ab008
Yusuke Yasuda <yusuke.yasuda03@g.softbank.co.jp>
2020-02-20 19:10:19 +0900

    BindingResultの勉強をした

--- src/main/java/com/example/demo_0219/DemoController.java
+++ src/main/java/com/example/demo_0219/DemoController.java
@@ -1,19 +1,63 @@
 package com.example.demo_0219;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.constraints.Size;
-import java.util.Map;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
     @PostMapping("/")
-    public String foo(@RequestBody @Validated Target target, BindingResult result) {
-        return target.getFoo() + " : " + target.getBar() + "\n";
+    public ResponseEntity<String> foo(@RequestBody @Validated Target target, BindingResult result) {
+
+        String body = "----------結果----------\n";
+        int httpStatusCode = 200;
+
+        if (result.getFieldErrorCount() == 0) {
+            body = body + "エラーは有りませんでした。\n"
+                    + "foo: " + target.getFoo() + "\n"
+                    + "bar: " + target.getBar() + "\n"
+                    + "baz: " + target.getBaz() + "\n"
+                    + "qux: " + target.getQux() + "\n";
+        } else {
+//            System.out.println("----------------------------------");
+//        System.out.println(result.getAllErrors());
+//        System.out.println("Model: " + result.getModel());
+//        System.out.println(result.getPropertyEditorRegistry());
+//        System.out.println(result.getSuppressedFields());
+//            System.out.println(result.getFieldErrorCount());
+//            System.out.println(result.getErrorCount());
+//            System.out.println(result.getFieldError()); //どれか一個だけ
+//        System.out.println(result.getFieldError().hashCode()); //エラーがないとヌルポが出る
+//        System.out.println(result.getFieldError().isBindingFailure());
+//            System.out.println(result.getFieldError("qux").getField()); //エラーがないとヌルポが出る
+//        System.out.println(result.getFieldError().getRejectedValue());
+//            System.out.println(result.getFieldErrors()); //一覧をListで
+//            System.out.println(result.getFieldErrors().get(0).getField()); //一覧をListで
+//            System.out.println(result.getFieldErrors().get(0).isBindingFailure());
+//            System.out.println(result.getFieldErrors().get(0).getRejectedValue());
+//        System.out.println("Target:" + result.getTarget());
+//        System.out.println("GlobalErrors: " + result.getGlobalErrors());
+
+            String fields = "---------エラー箇所---------\n";
+
+            for (int i = 0; i < result.getFieldErrorCount(); i++) {
+                fields = fields
+                        + i + "番目の理由: " + result.getFieldErrors().get(i).getDefaultMessage() + "\n"
+                        + i + "番目の中身 - " + result.getFieldErrors().get(i).getField() + ": " + result.getFieldErrors().get(i).getRejectedValue() + "\n";
+            }
+
+            body = body
+                    + "「" + result.getObjectName() + "」オブジェクトで、\n"
+                    + result.getErrorCount() + "件のエラーが有りました。\n"
+                    + fields + "\n";
+            httpStatusCode = 400;
+        }
+
+        return new ResponseEntity<>(body, HttpStatus.valueOf(httpStatusCode));
     }
-
-
 }
--- src/main/java/com/example/demo_0219/Target.java
+++ src/main/java/com/example/demo_0219/Target.java
@@ -1,12 +1,24 @@
 package com.example.demo_0219;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Target {
-    @Size(max = 16)
+
+    @Size(max = 5)
     public String foo;
 
-//    @Size(max = 20)
+    @NotNull
+    public String bar;
+
+    @Pattern(regexp = "\\d{5}")
+    public String baz;
+
+    @Max(5)
+    public String qux;
+
     public void setFoo(String foo) {
         this.foo = foo;
     }
@@ -15,7 +27,14 @@ public class Target {
         this.bar = bar;
     }
 
-    @Size(max = 10)
+    public void setBaz(final String baz) {
+        this.baz = baz;
+    }
+
+    public void setQux(final String qux) {
+        this.qux = qux;
+    }
+
     public String getFoo() {
         return foo;
     }
@@ -24,5 +43,11 @@ public class Target {
         return bar;
     }
 
-    public String bar;
+    public String getBaz() {
+        return baz;
+    }
+
+    public String getQux() {
+        return qux;
+    }
 }
